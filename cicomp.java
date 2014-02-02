db5f6febae2b4eedb00bb7cc37117358bb622f0a
Matt Ahlschwede <perscrumption@hotmail.com>
2014-02-02 16:47:10 -0600

    Update cicomp.java

--- cicomp.java
+++ cicomp.java
@@ -119,24 +119,44 @@ public void setCell(cell hammurabi){
 	
 public void refCell(){
 	moniker.setText(xerxes.getName());
+	// set labels for Aging
 	if(xerxes.getOption("Ages")){fields[1].setVisible(true); aLabel.setText(Integer.toString(xerxes.getParameter("Age"))); aLabel.setVisible(true);}
 	else{fields[1].setVisible(false); aLabel.setVisible(false);}
+	//set labels for Fade rule
 	if(xerxes.getOption("Fades")){fields[2].setVisible(true); fLabel.setText(Integer.toString(xerxes.getParameter("Fade"))); fLabel.setVisible(true);}
 	else{fields[2].setVisible(false); fLabel.setVisible(false);}
+	//set labels for 4-orientations
 	if(xerxes.getControls("Orient")){fields[3].setText("Orientation: ");fields[3].setVisible(true); dLabel.setText(Integer.toString(xerxes.getParameter("Dir")));
 		if(xerxes.getOption("Any")){dLabel.setText("Any");}if(xerxes.getOption("All")){dLabel.setText("All");}
-		dLabel.setVisible(true);}
+		dLabel.setVisible(true);}else{fields[3].setVisible(false); dLabel.setVisible(false);}
+	//set labels for 8-directions
+	if(xerxes.getControls("Dir")){fields[3].setText("Direction: ");fields[3].setVisible(true);
+		switch(xerxes.getParameter("Dir")){
+			case 0 : dLabel.setText("Up"); break;
+			case 1 : dLabel.setText("Upper-Right"); break;
+			case 2 : dLabel.setText("Right"); break;
+			case 3 : dLabel.setText("Lower-Right"); break;
+			case 4 : dLabel.setText("Down"); break;
+			case 5 : dLabel.setText("Lower-Left"); break;
+			case 6 : dLabel.setText("Left"); break;
+			case 7 : dLabel.setText("Upper-Left"); break;
+			default: dLabel.setText("ERROR"); break;}
+			dLabel.setVisible(true);}
 	else{fields[3].setVisible(false); dLabel.setVisible(false);}
+	// Labels for Maturity
 	if(xerxes.getControls("Mat")){fields[4].setVisible(true); mcLabel.setText(Integer.toString(xerxes.getParameter("Matcount"))); mtLabel.setText("/" + Integer.toString(xerxes.getParameter("Mat"))); mcLabel.setVisible(true); mtLabel.setVisible(true);}
 	else{fields[4].setVisible(false); mcLabel.setVisible(false); mtLabel.setVisible(false);}
+	// labels for MBOT rules
 	if(xerxes.getControls("Born")){fields[6].setVisible(true);String bstr = ""; for(int i = 0; i < 9; i++){String[] bbs = new String[]{"B0","B1","B2","B3","B4","B5","B6","B7","B8"};
 	if(xerxes.getOption(bbs[i])){bstr = bstr + Integer.toString(i);}}bLabel.setText(bstr);bLabel.setVisible(true);}
 	else{fields[6].setVisible(false); bLabel.setVisible(false);}
 	if(xerxes.getControls("Survives")){fields[7].setVisible(true);String sstr = ""; for(int v = 0; v < 9; v++){String[] sss = new String[]{"S0","S1","S2","S3","S4","S5","S6","S7","S8"};
 	if(xerxes.getOption(sss[v])){sstr = sstr+ Integer.toString(v);}}sLabel.setText(sstr);sLabel.setVisible(true); }
 	else{fields[7].setVisible(false);sLabel.setVisible(false);}
+	//labels for Wolfram rule
 	if(xerxes.getControls("WolfRule")){fields[5].setVisible(true);wrLabel.setText(Integer.toString(xerxes.getParameter("WolfRule")));wrLabel.setVisible(true);}
 	else{fields[5].setVisible(false);wrLabel.setVisible(false);}
+	//labels for Mirroring
 	if(xerxes.getOption("Mirror")){fields[8].setVisible(true); mxLabel.setText(Integer.toString(xerxes.getParameter("MirrX"))); mxLabel.setVisible(true); 
 		fields[9].setVisible(true); myLabel.setText(Integer.toString(xerxes.getParameter("MirrY"))); myLabel.setVisible(true);}
 	else{fields[8].setVisible(false); mxLabel.setVisible(false); fields[9].setVisible(false); myLabel.setVisible(false);}
