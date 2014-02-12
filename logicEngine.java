071db1f1872919e21ea7dc9a95f6bb9b93059620
Matt Ahlschwede <perscrumption@hotmail.com>
2014-02-12 02:40:33 -0600

    Update logicEngine.java

--- logicEngine.java
+++ logicEngine.java
@@ -11,4 +11,5 @@ public void iterateInterrupt(int a){}
 public void setMasterSpeed(int a){}
 public int getMasterSpeed(){return 0;}
 public void iterateNotify(){}
+public void updateState(boolean[][] uds, cellBrain mybrain){}
 }
