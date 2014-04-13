82dbd4cfa7bd13c53d59a3797dec96e2c73fae10
Matt Ahlschwede <perscrumption@hotmail.com>
2014-04-13 00:59:59 -0500

    Update wolfram.java

--- wolfram.java
+++ wolfram.java
@@ -161,9 +161,10 @@ public class wolfram extends cell{
 			 matcount += 1;
 			 if(matcount >= mat){matcount = 0;
 			 calculate(); }
-			 if(ages){ if(active){ if(age == 0){age = 1;} else{age += 1;}}else{ age = 0;} state = age;}
+			 if(ages){ if(active){ if(age == 0){age = 1;} else{age += 1;}}else{ age = 0;} 
+				if(fades){ if( age >= fade){ purgeState(); age = 0;}}
+				state = agify(age);}
 			 else{if(active){state = 1;}else{state = 0;}}
-			 if(fades){ if( age >= fade){ purgeState(); age = 0;}}
 			
 			}
 		
