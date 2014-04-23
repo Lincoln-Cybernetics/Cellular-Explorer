d990e3633b1f51cd0c36a298e4d55eabdff892ab
Matt Ahlschwede <perscrumption@hotmail.com>
2014-04-23 02:31:52 -0500

    Update cellOptionHandler.java

--- cellOptionHandler.java
+++ cellOptionHandler.java
@@ -128,6 +128,7 @@ public cell generateCell(){
 		case 7: tiamat = new symmetriCell(); break;
 		case 8: tiamat = new conveyorCell(); break;
 		case 9: tiamat = new strobeCell(); break;
+		case 10: tiamat = new totalCell(); break;
 		default: tiamat = new cell();break;}
 		// set options and parameters
 		if(tiamat.getControls("Age")){ tiamat.setOption("Ages", doesage);}
@@ -218,6 +219,7 @@ class randcellOptionHandler extends cellOptionHandler{
 		case 7: tiamat = new symmetriCell();break;
 		case 8: tiamat = new conveyorCell(); break;
 		case 9: tiamat = new strobeCell(); break;
+		case 10: tiamat = new totalCell(); break;
 		default: tiamat = new cell();break;}
 		
 		// randomly set mirror options
@@ -257,12 +259,13 @@ class randcellOptionHandler extends cellOptionHandler{
 	
 	public cell getCell(){
 		int cellgen = shovel.nextInt(1024);
-		if(cellgen == 0 || cellgen == 1){celltype = 5;}//offcell
-		if(cellgen > 1 && cellgen < 4){celltype = 4;}//oncell
-		if(cellgen > 3 && cellgen < 8){celltype = 6;}//blinkcell
-		if(cellgen > 7 && cellgen < 16){celltype = 9;}//strobe cell
-		if(cellgen > 15 && cellgen < 32){celltype = 3;}//randcell
-		if(cellgen > 31 && cellgen < 64){celltype = 0;}//cell
+		if(cellgen == 0 ){celltype = 5;}//offcell
+		if(cellgen == 1){celltype = 4;}//oncell
+		if(cellgen > 1 && cellgen < 4){celltype = 6;}//blinkcell
+		if(cellgen > 3 && cellgen < 8){celltype = 9;}//strobe cell
+		if(cellgen > 7 && cellgen < 16){celltype = 3;}//randcell
+		if(cellgen > 15 && cellgen < 32){celltype = 0;}//cell
+		if(cellgen > 31 && cellgen < 64){celltype = 10;}//total cell
 		if(cellgen > 63 && cellgen < 128){celltype = 8;}//conveyorcell
 		if(cellgen > 127 && cellgen < 256){celltype = 1;}//Wolfram
 		if(cellgen > 255 && cellgen < 512){celltype = 7;}//symmetric
