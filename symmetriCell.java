dccc787f8a8394f9aaa9ec05702326c2c48f1fe3
Matt Ahlschwede <perscrumption@hotmail.com>
2014-04-30 03:09:54 -0500

    Update symmetriCell.java

--- symmetriCell.java
+++ symmetriCell.java
@@ -154,7 +154,7 @@ public class symmetriCell extends cell{
 			 calculate(); }
 			 if(ages){ if(active){ if(age == 0){age = 1;} else{age += 1;}}else{ age = 0;} 
 				if(fades){ if( age >= fade){ purgeState(); age = 0;}}
-				if( age > 1023){ age = 512; }state = agify(age);}
+				if( age > 1023){ age = 1023; }state = agify(age);}
 			 else{if(active){state = 1;}else{state = 0;}}
 			
 			}
